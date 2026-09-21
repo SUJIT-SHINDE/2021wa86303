d173a8f8c30ce8497f92e80e1e404a7ce967c615
Sujit-Shinde <2021wa86303@wilp.bits-pilani.ac.in>
2026-09-21 17:08:00 +0530

    Initial DevOps Maven project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Maven Project!");
+        System.out.println("Continuous Integration using Jenkins and GitHub.");
+    }
+
+}
