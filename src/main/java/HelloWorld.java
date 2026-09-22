2c412bcacd991767bc230b70129dcb351ddf3d76
Sujit-Shinde <2021wa86303@wilp.bits-pilani.ac.in>
2026-09-22 16:05:04 +0530

    Modify source code for continuous integration

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Maven Project!");
+        System.out.println("Hello from Continuous Integration!");
         System.out.println("Continuous Integration using Jenkins and GitHub.");
     }
 
